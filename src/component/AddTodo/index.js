a2197159f08b8572784515cae8fd5dc5877dff0a
lalalazero <zzzero520@hotmail.com>
2019-01-26 22:48:05 +0800

    更新 iconfont； 添加待办事项增加 星标 和 日期选择 icon；

--- src/component/AddTodo/index.js
+++ src/component/AddTodo/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './style.scss'
+import { DatePicker } from 'antd';
 
 export default class AddTodo extends Component {
     constructor(props){
@@ -8,23 +9,32 @@ export default class AddTodo extends Component {
             value: ''
         }
     }
-    handleChange = (e) => {
+    onChange = (e) => {
         this.setState({
             value: e.target.value
         })
     }
-    handleSubmit = (e) => {
+    onKeyDown = (e) => {
         if(e.keyCode === 13) {
             console.log('按下 anter 键..准备提交')
         }
+    }
+    onDatePick = (e)=>{
+        console.log('date change..', e.target.value)
     }
     render(){
         const { value } = this.state
         return (
             <div className='addTodo-container'>
                 <i className='iconfont icon-plus'></i>
-                <input placeholder='添加待办...' value={value} onChange={this.handleChange} onKeyDown={this.handleSubmit}></input>
+                <input placeholder='添加待办...' value={value} onChange={this.onChange} onKeyDown={this.onKeyDown}></input>
+                <i className='iconfont icon-star'></i>
+                <i className='iconfont icon-canlender'></i>
+                <div className='datePanel'>
+                    这里是手写日期
+                </div>
             </div>
+            
         )
     }
 }
